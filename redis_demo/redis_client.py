eb5768813dd52732f15598d9efbc1673fbc147db
Juan Henrique Carneiro Aguiar Paes <jhcap@windows.cin.ufpe.br>
2025-07-30 16:36:39 -0300

    POOL conection with redis

--- redis_demo/redis_client.py
+++ redis_demo/redis_client.py
@@ -1,4 +1,6 @@
 import redis
 
+POOL = redis.ConnectionPool(host='redis-demo', port=6379, decode_responses=True)
+
 def get_redis():
-    return redis.Redis(host='redis-demo', port=6379, decode_responses=True)
+    return redis.Redis(connection_pool=POOL)
